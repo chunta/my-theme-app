685d98110ca03145e64c238365d2d5ebf95040c6
DESKTOP-NSKQVF8\user <hi.chuntachen@gmail.com>
2025-08-11 19:52:45 +0800

    tmp

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,10 @@ import ThemeToggle from '@/components/ThemeToggle'
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center
-                     bg-white text-gray-900
-                     dark:bg-purple-700 dark:text-gray-100
-                     transition-colors duration-300">
-      <h1 className="text-4xl font-bold mb-6">
+    <main>
+      <div className='test'>
         Next.js App Router Theme Toggle2
-      </h1>
+      </div>
 
       <ThemeToggle />
     </main>
